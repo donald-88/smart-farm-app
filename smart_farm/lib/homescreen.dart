7cd2b5134fcd181a4ba78b384f7184158f9e9495
McDonald Namba <nambamcdonald@gmail.com>
2023-02-28 11:52:47 +0200

    created flutter app

---
+++ smart_farm/lib/homescreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Smart Farm"),
+        backgroundColor: Colors.grey,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("Temperature: "),
+            Text("Humidity: "),
+            Text("Light Intensity: ")
+          ],
+        ),
+      ),
+    );
+  }
+}
